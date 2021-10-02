bcc2720b49455659c9452de0f12b5433c6c73582
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-10-02 05:57:10 +0200

    new version 1.0.4

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,5 +3,5 @@ export * from './core-models';
 import { HelpersCore as Base } from './helpers';
 export * from './core-config';
 export * from './progress-data';
-export const Helpers = Base.InstanceCore;
 export { HelpersCore as CoreHelpers } from './helpers';
+export const Helpers = Base.InstanceCore;
