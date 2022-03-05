911e2fa7052952a9fc9f4e4793861601595a5b1a
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-06 00:17:54 +0100

    a

---
+++ src/lib/public-api.ts
@@ -0,0 +1,10 @@
+export * from './core-imports';
+export * from './core-models';
+import { HelpersCore as Base } from './helpers';
+export * from './core-config';
+export * from './progress-data';
+export { HelpersCore as CoreHelpers } from './helpers';
+export let Helpers = Base.InstanceCore;
+
+
+
