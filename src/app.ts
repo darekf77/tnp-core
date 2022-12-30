2aeb060c9755b0d91972c26c33942c7c543bcf93
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-12-30 03:56:10 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,130 @@
 // #region @notForNpm
-
-import { Helpers } from './lib';
-
-
-console.log(Helpers.values({ test: 'testvalue' }))
-
+import { Firedev } from 'firedev';
+import { map, Observable, share, Subject, takeUntil, tap } from 'rxjs';
+const host = `http://localhost:3232`;
 
 //#region @browser
-import { NgModule } from '@angular/core';
-
+import { NgModule, NgZone } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//#endregion
+@Firedev.Entity({ className: 'Task' })
+class Task extends Firedev.Base.Entity {
 
+  //#region @websql
+
+  @Firedev.Orm.Column.Generated()
+  //#endregion
+  id?: number;
+
+  //#region @websql
+
+  @Firedev.Orm.Column.Custom('varchar')
+  //#endregion
+  name?: string;
+
+  static from(data: Omit<Task, 'id' | 'ctrl'>) {
+    return Object.assign(new Task(), data) as Task;
+  }
+  static ctrl: TaskController;
+}
+
+@Firedev.Controller({ className: 'TaskController', entity: Task })
+class TaskController extends Firedev.Base.Controller<Task> {
+  //#region @websql
+  async initExampleDbData() {
+    const db = await this.connection.getRepository(Task);
+    await db.save(Task.from({ name: 'my first tasks todo' }));
+    await db.save(Task.from({ name: 'my second tasks todo' }));
+    await db.save(Task.from({ name: 'third task goes to Artur!' }));
+  }
+  //#endregion
+
+  @Firedev.Http.GET()
+  count(): Firedev.Response<number> {
+    //#region @websqlFunc
+    return async () => {
+      return 3;
+    }
+    //#endregion
+  }
+}
+
+//#region @browser
 @Component({
   selector: 'app-tnp-core',
-  template: `hello`
+  template: `
+    <ul *ngIf="tasks$" >
+      Tasks:
+      <li *ngFor="let task of (tasks$ | async)" > - {{ task.name }} </li>
+    </ul>
+    <div *ngIf="count$" >
+    task count from server: {{ (count$ | async) | json }}
+    </div>
+  `
 })
-
 export class TnpCoreComponent implements OnInit {
-  constructor() { }
+  destroy$ = new Subject();
+  tasks$: Observable<Task[]>;
+  count$: Observable<number>;
+  normalTasks = [];
+  constructor(ngzone: NgZone) {
+    Firedev.initNgZone(ngzone);
+  }
 
-  ngOnInit() { }
+  async ngOnInit() {
+    await start();
+
+    this.tasks$ = Task.ctrl.getAll().received.observable.pipe(
+      takeUntil(this.destroy$),
+      map(r => {
+        const tasks = r.body.json;
+        console.log({ tasks })
+        return tasks;
+      }),
+      share(),
+    );
+
+    this.count$ = Task.ctrl.count().received.observable.pipe(
+      takeUntil(this.destroy$),
+      map(r => {
+        const count = Number(r.body.text);
+        return count;
+      }),
+    );
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next(void 0);
+    this.destroy$.unsubscribe();
+  }
 }
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   exports: [TnpCoreComponent],
   declarations: [TnpCoreComponent],
-  providers: [],
 })
 export class TnpCoreModule { }
 //#endregion
 
-export default function () {
-  console.log('hello')
+async function start() {
+  const config = await Firedev.init({
+    host,
+    controllers: [TaskController],
+    entities: [Task],
+    //#region @websql
+    config: {
+      type: 'better-sqlite3',
+      database: 'tmp-db1.sqlite',
+      synchronize: true,
+      dropSchema: true,
+      logging: false,
+    }
+    //#endregion
+  });
+
+  console.log(config);
 }
 
+export default start;
 //#endregion
