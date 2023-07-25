c5651bbf1301e16b5c0f7daf4f2c8683f2ab88c7
Dariusz Filipiak <darekf77@gmail.com>
2023-07-25 20:59:51 +0200

    progressing with binary transfer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@ export * from './core-models';
 import { HelpersCore as Base } from './helpers';
 export * from './core-config';
 export * from './progress-data';
+export * from './utils';
 export { HelpersCore as CoreHelpers, CommandOutputOptions } from './helpers';
 export let Helpers = Base.InstanceCore;
 
---
+++ src/lib/utils.ts
@@ -0,0 +1,170 @@
+import { ContentType, mimeTypes } from "./core-models";
+import axios, { AxiosResponse } from 'axios';
+import { path } from "./core-imports";
+//#region @backend
+import { Blob } from 'buffer';
+//#endregion
+
+export namespace Utils {
+
+  export namespace binary {
+
+    //#region binay utils / array buffer to blob
+    //#region @browser
+    export async function arrayBufferToBlob(buffer: ArrayBuffer, contentType: ContentType): Promise<Blob> {
+      // @ts-ignore
+      return new Blob([buffer], { type: contentType });
+    }
+    //#endregion
+    //#endregion
+
+    //#region binay utils / blob to array buffer
+    export async function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer> {
+      return await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.addEventListener('loadend', () => {
+          resolve(reader.result as any);
+        });
+        reader.addEventListener('error', reject); // @ts-ignore
+        reader.readAsArrayBuffer(blob);
+      });
+    }
+    //#endregion
+
+    //#region binay utils / blob to base64 string
+    /**
+     * it is revers to base64toBlob
+     * @param blob
+     * @returns
+     */
+    export function blobToBase64(blob: Blob): Promise<string> {
+      return new Promise<any>((resolve, _) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result); // @ts-ignore
+        reader.readAsDataURL(blob);
+      });
+    }
+    //#endregion
+
+    //#region binay utils / base64 string to blob
+    /**
+     * it is revers to blobToBase64()
+     * @param base64Data
+     * @returns
+     */
+    export async function base64toBlob(base64Data: string, contentTypeOverride?: ContentType): Promise<Blob> {
+      let content_type: ContentType = void 0 as any;
+      let file_base64: string = void 0 as any;
+      if (!contentTypeOverride) {
+        const m = /^data:(.+?);base64,(.+)$/.exec(base64Data);
+        if (!m) {
+          throw new Error(`[firedev-framework][base64toBlob] Not a base64 blob [${base64Data}]`)
+        }
+        // tslint:disable-next-line:prefer-const
+        var [__, contenttype, filebase64] = m;
+        content_type = contenttype as any;
+        file_base64 = filebase64;
+      }
+      content_type = (contentTypeOverride ? contentTypeOverride : content_type || '') as any;
+      base64Data = contentTypeOverride ? base64Data : file_base64;
+      const sliceSize = 1024;
+      const byteCharacters = atob(base64Data);
+      const bytesLength = byteCharacters.length;
+      const slicesCount = Math.ceil(bytesLength / sliceSize);
+      const byteArrays = new Array(slicesCount);
+
+      for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+        const begin = sliceIndex * sliceSize;
+        const end = Math.min(begin + sliceSize, bytesLength);
+
+        const bytes = new Array(end - begin);
+        // tslint:disable-next-line:one-variable-per-declaration
+        for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+          bytes[i] = byteCharacters[offset].charCodeAt(0);
+        }
+        byteArrays[sliceIndex] = new Uint8Array(bytes);
+      }
+      return new Blob(byteArrays, { type: content_type });
+    }
+    //#endregion
+
+    //#region binay utils / file to blob
+    export async function fileToBlob(file: File) {
+      return new Blob([new Uint8Array(await file.arrayBuffer())], { type: file.type })
+    };
+    //#endregion
+
+    //#region binay utils / blob to file
+    export async function blobToFile(blob: Blob, nameForFile: string = 'my-file-name'): Promise<File> {
+      if (!nameForFile) {
+        nameForFile = 'nonamefile' + (new Date()).getTime();
+      };
+      // @ts-ignore
+      return new File([blob], nameForFile);
+    }
+    //#endregion
+
+    //#region binay utils / get blob from url
+    export async function getBlobFrom(url: string): Promise<Blob> {
+      const response: AxiosResponse<Blob> = await axios({
+        url,
+        method: 'get',
+        responseType: 'blob'
+      });
+      return response.data;
+    }
+    //#endregion
+
+    export async function textToBlob(text: string, type: ContentType = 'text/plain'): Promise<Blob> {
+      const blob = new Blob([text], { type });
+      return blob;
+    }
+
+    export async function textToFile(text: string, fileRelativePathOrName: string): Promise<File> {
+      const type = mimeTypes[path.extname(fileRelativePathOrName)];
+      const blob = new Blob([text], { type });
+      return await blobToFile(blob, fileRelativePathOrName);
+    }
+
+    export async function fileToText(file: File): Promise<string> {
+      return await file.text();
+    }
+
+    export async function blobToText(blob: Blob): Promise<string> {
+      return await blob.text()
+    }
+
+    export async function jsonToBlob(jsonObj: object): Promise<Blob> {
+      const blob = new Blob([JSON.stringify(jsonObj, null, 2)], { type: 'application/json' });
+      return blob;
+    }
+
+    /**
+     * TODO NOT TESTED
+     */
+    export async function blobToJson(blob: Blob): Promise<string> {
+      return JSON.parse(await blob.text())
+    }
+
+  }
+
+  export namespace css {
+    //#region css utils / numeric value of pixels
+    /**
+     *
+     * @param pixelsCss pixels ex. 100px
+     * @returns
+     */
+    export function numValue(pixelsCss: string) {
+      // tslint:disable-next-line:radix
+      return parseInt(pixelsCss
+        .replace('px', '')
+        // .replace('pt', '') TOOD handle other types
+        // .replace('1rem', '') // to
+      );
+    }
+    //#endregion
+  }
+
+
+}
