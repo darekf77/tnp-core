509c9f5c548ebf8be4999d24cc2b9a7e271861d5
Dariusz Filipiak <darekf77@gmail.com>
2025-02-25 21:58:09 +0100

    feat: util required uncached

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -113,6 +113,66 @@ export namespace Utils {
   };
   //#endregion
 
+  //#region utils / required uncached
+  /**
+   * Traverses the cache to search for all the cached
+   * files of the specified module name
+   */
+  const searchCache = (moduleName, callback) => {
+    //#region @backendFunc
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName) as any;
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && (mod = require.cache[mod]) !== undefined) {
+      // Recursively go over the results
+      (function traverse(mod) {
+        // Go over each of the module's children and
+        // traverse them
+        mod['children'].forEach(function (child) {
+          traverse(child);
+        });
+
+        // Call the specified callback providing the
+        // found cached module
+        callback(mod);
+      })(mod);
+    }
+    //#endregion
+  };
+
+  /**
+   * Removes a module from the cache
+   */
+  const purgeCache = moduleName => {
+    //#region @backendFunc
+    // Traverse the cache looking for the files
+    // loaded by the specified module name
+    searchCache(moduleName, function (mod) {
+      delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor['_pathCache']).forEach(function (cacheKey) {
+      if (cacheKey.indexOf(moduleName) > 0) {
+        delete module.constructor['_pathCache'][cacheKey];
+      }
+    });
+    //#endregion
+  };
+
+  export const requireUncached = (module: string) => {
+    //#region @backendFunc
+    const result = _.cloneDeep(require(module));
+    purgeCache(module);
+    return result;
+    //#endregion
+  };
+
+  //#endregion
+
   //#region json
   interface AttrJsoncProp {
     name: string;
