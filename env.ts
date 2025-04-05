1fff24ecfe4af0633fd0da7a56dd3553ed723032
Dariusz Filipiak <darekf77@gmail.com>
2025-04-05 15:16:15 +0200

    $$$ update $$$

---
+++ env.ts
@@ -0,0 +1,10 @@
+// @taon-ignore
+// import type { EnvOptions } from '../../../../../../lib/src';
+
+const env: Partial<any> = {
+  website: {
+    domain: 'tnp-core.example.domain.com',
+    title: 'Tnp Core',
+  },
+};
+export default env;
