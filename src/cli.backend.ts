ef10e2a35186535b05be96358638379a1774c545
Dariusz Filipiak <darekf77@gmail.com>
2025-04-22 16:24:24 +0200

    chore: update

--- src/cli.backend.ts
+++ src/cli.backend.ts
@@ -1,17 +1,17 @@
-//#region @notForNpm
-import { Helpers } from './lib/index';
-import { child_process } from './lib/index';
-
-try {
-  const out = child_process
-    .execSync('where taon', { stdio: ['ignore', 'pipe', 'ignore'] })
-    ?.toString();
-  console.log({
-    out,
-  });
-} catch (error) {
-  console.log({
-    error,
-  });
-}
-//#endregion
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+  
