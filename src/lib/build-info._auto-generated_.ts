87547e6bed839cbbcbda780b919dffac736fefd1
Dariusz Filipiak <darekf77@gmail.com>
2025-05-22 22:51:31 +0200

    release: v19.0.31

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const PROJECT_NPM_NAME = 'tnp-core';
-export const CURRENT_PACKAGE_VERSION = '19.0.30';
+export const CURRENT_PACKAGE_VERSION = '19.0.31';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
