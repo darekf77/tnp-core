e17a3b748604c708e0fefe6fd7e804bb491fb434
Dariusz Filipiak <darekf77@gmail.com>
2025-07-30 16:21:43 +0200

    chore: update

--- src/app.ts
+++ src/app.ts
@@ -1,132 +1,194 @@
-// //#region imports
-// import { CommonModule } from '@angular/common';
-// import { NgModule, inject, Injectable } from '@angular/core';
-// import { Component, OnInit } from '@angular/core';
-// import { VERSION } from '@angular/core';
-// import { Observable, map } from 'rxjs';
-// import { Taon, BaseContext } from 'taon/src';
-// import { Helpers } from 'tnp-core/src';
+//#region imports
+import * as os from 'os'; // @backend
 
-// import { CLIENT_DEV_WEBSQL_APP_PORT, HOST_BACKEND_PORT } from './app.hosts';
-// //#endregion
+import { CommonModule } from '@angular/common'; // @browser
+import { NgModule, inject, Injectable } from '@angular/core'; // @browser
+import { Component, OnInit } from '@angular/core'; // @browser
+import { VERSION } from '@angular/core'; // @browser
+import Aura from '@primeng/themes/aura'; // @browser
+import { MaterialCssVarsModule } from 'angular-material-css-vars'; // @browser
+import { providePrimeNG } from 'primeng/config'; // @browser
+import { Observable, map } from 'rxjs';
+import { Taon, BaseContext, TAON_CONTEXT } from 'taon/src';
+import { UtilsOs } from 'tnp-core/src';
 
+import { HOST_URL, FRONTEND_HOST_URL } from './app.hosts';
+import { APP_ID } from './lib/build-info._auto-generated_';
+//#endregion
 
-// console.log('hello world');
-// console.log('Your server will start on port ' + HOST_BACKEND_PORT);
-// const host = 'http://localhost:' + HOST_BACKEND_PORT;
+console.log('hello world');
+console.log('Your server will start on port ' + HOST_URL.split(':')[2]);
 
-// const frontendHost =
-//   'http://localhost:' +
-//   (Helpers.isWebSQL ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_WEBSQL_APP_PORT);
+//#region tnp-core component
+//#region @browser
+@Component({
+  selector: 'app-tnp-core',
+  standalone: false,
+  template: `hello from tnp-core<br />
+    Angular version: {{ angularVersion }}<br />
+    <br />
+    users from backend
+    <ul>
+      <li *ngFor="let user of users$ | async">{{ user | json }}</li>
+    </ul>
+    hello world from backend: <strong>{{ hello$ | async }}</strong> `,
+  styles: [
+    `
+      body {
+        margin: 0px !important;
+      }
+    `,
+  ],
+})
+export class TnpCoreComponent {
+  angularVersion =
+    VERSION.full +
+    ` mode: ${UtilsOs.isRunningInWebSQL() ? ' (websql)' : '(normal)'}`;
+  userApiService = inject(UserApiService);
+  readonly users$: Observable<User[]> = this.userApiService.getAll();
+  readonly hello$ = this.userApiService.userController
+    .helloWorld()
+    .received.observable.pipe(map(r => r.body.text));
+}
+//#endregion
+//#endregion
 
-// //#region tnp-core component
-// //#region @browser
-// @Component({
-//   selector: 'app-tnp-core',
-//   template: `hello from tnp-core<br />
-//     Angular version: {{ angularVersion }}<br />
-//     <br />
-//     users from backend
-//     <ul>
-//       <li *ngFor="let user of users$ | async">{{ user | json }}</li>
-//     </ul> `,
-//   styles: [
-//     `
-//       body {
-//         margin: 0px !important;
-//       }
-//     `,
-//   ],
-// })
-// export class TnpCoreComponent {
-//   angularVersion = VERSION.full;
-//   userApiService = inject(UserApiService);
-//   readonly users$: Observable<User[]> = this.userApiService.getAll();
-// }
-// //#endregion
-// //#endregion
+//#region  tnp-core api service
+//#region @browser
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends Taon.Base.AngularService {
+  userController = this.injectController(UserController);
+  getAll(): Observable<User[]> {
+    return this.userController
+      .getAll()
+      .received.observable.pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+//#endregion
 
-// //#region  tnp-core api service
-// //#region @browser
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class UserApiService {
-//   userControlller = Taon.inject(() => MainContext.getClass(UserController));
-//   getAll() {
-//     return this.userControlller
-//       .getAll()
-//       .received.observable.pipe(map(r => r.body.json));
-//   }
-// }
-// //#endregion
-// //#endregion
+//#region  tnp-core module
+//#region @browser
+@NgModule({
+  providers: [
+    {
+      provide: TAON_CONTEXT,
+      useFactory: () => MainContext,
+    },
+    providePrimeNG({
+      // inited ng prime - remove if not needed
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+  exports: [TnpCoreComponent],
+  imports: [
+    CommonModule,
+    MaterialCssVarsModule.forRoot({
+      // inited angular material - remove if not needed
+      primary: '#4758b8',
+      accent: '#fedfdd',
+    }),
+  ],
+  declarations: [TnpCoreComponent],
+})
+export class TnpCoreModule {}
+//#endregion
+//#endregion
 
-// //#region  tnp-core module
-// //#region @browser
-// @NgModule({
-//   exports: [TnpCoreComponent],
-//   imports: [CommonModule],
-//   declarations: [TnpCoreComponent],
-// })
-// export class TnpCoreModule {}
-// //#endregion
-// //#endregion
+//#region  tnp-core entity
+@Taon.Entity({ className: 'User' })
+class User extends Taon.Base.AbstractEntity {
+  //#region @websql
+  @Taon.Orm.Column.String()
+  //#endregion
+  name?: string;
+}
+//#endregion
 
-// //#region  tnp-core entity
-// @Taon.Entity({ className: 'User' })
-// class User extends Taon.Base.AbstractEntity {
-//   //#region @websql
-//   @Taon.Orm.Column.String()
-//   //#endregion
-//   name?: string;
-// }
-// //#endregion
+//#region  tnp-core controller
+@Taon.Controller({ className: 'UserController' })
+class UserController extends Taon.Base.CrudController<User> {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  entityClassResolveFn = () => User;
 
-// //#region  tnp-core controller
-// @Taon.Controller({ className: 'UserController' })
-// class UserController extends Taon.Base.CrudController<User> {
-//   entityClassResolveFn = () => User;
-//   //#region @websql
-//   async initExampleDbData(): Promise<void> {
-//     const superAdmin = new User();
-//     superAdmin.name = 'super-admin';
-//     await this.db.save(superAdmin);
-//   }
-//   //#endregion
-// }
-// //#endregion
+  @Taon.Http.GET()
+  helloWorld(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => 'hello world';
+    //#endregion
+  }
 
-// //#region  tnp-core context
-// var MainContext = Taon.createContext(() => ({
-//   host,
-//   frontendHost,
-//   contextName: 'MainContext',
-//   contexts: { BaseContext },
-//   controllers: {
-//     UserController,
-//     // PUT FIREDEV CONTORLLERS HERE
-//   },
-//   entities: {
-//     User,
-//     // PUT FIREDEV ENTITIES HERE
-//   },
-//   database: true,
-//   // disabledRealtime: true,
-// }));
-// //#endregion
+  @Taon.Http.GET()
+  getOsPlatform(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => {
+      //#region @backend
+      return os.platform(); // for normal nodejs backend return real value
+      //#endregion
+      return 'no-platform-inside-browser-and-websql-mode';
+    };
+    //#endregion
+  }
+}
+//#endregion
 
-// async function start() {
-//   await MainContext.initialize();
+//#region  tnp-core migration
+//#region @websql
+@Taon.Migration({
+  className: 'UserMigration',
+})
+class UserMigration extends Taon.Base.Migration {
+  userController = this.injectRepo(User);
+  async up(): Promise<any> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.userController.save(superAdmin);
+  }
+}
+//#endregion
+//#endregion
 
-//   if (Taon.isBrowser) {
-//     const users = (
-//       await MainContext.getClassInstance(UserController).getAll().received
-//     ).body?.json;
-//     console.log({
-//       'users from backend': users,
-//     });
-//   }
-// }
+//#region  tnp-core context
+var MainContext = Taon.createContext(() => ({
+  host: HOST_URL,
+  appId: APP_ID,
+  frontendHost: FRONTEND_HOST_URL,
+  contextName: 'MainContext',
+  contexts: { BaseContext },
+  //#region @websql
+  migrations: {
+    UserMigration,
+  },
+  //#endregion
+  controllers: {
+    UserController,
+  },
+  entities: {
+    User,
+  },
+  database: true,
+  // disabledRealtime: true,
+}));
+//#endregion
 
-// export default start;
+async function start(): Promise<void> {
+  await MainContext.initialize();
+  //#region @backend
+  console.log(`Hello in NodeJs backend! os=${os.platform()}`);
+  //#endregion
+
+  if (UtilsOs.isBrowser) {
+    const users = (
+      await MainContext.getClassInstance(UserController).getAll().received
+    ).body?.json;
+    console.log({
+      'users from backend': users,
+    });
+  }
+}
+
+export default start;
