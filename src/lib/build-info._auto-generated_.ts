28b1652bf029839238abfdfdefb88a9976e7644e
Dariusz Filipiak <darekf77@gmail.com>
2025-08-09 09:11:10 +0200

    $$$ update $$$

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,5 +1,5 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
-export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
+export const BUILD_FRAMEWORK_CLI_NAME = 'taon';
 export const APP_ID = 'com.domain.example.tnp-core';
 export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'tnp-core';
