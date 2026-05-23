2ce239b0955381ee4e8168ca060657d7833981b1
Dariusz Filipiak <darekf77@gmail.com>
2026-05-23 17:30:20 +0200

    fix: use in memory json file preset db

--- src/lib/lowdb/presets/node.ts
+++ src/lib/lowdb/presets/node.ts
@@ -1,33 +1,33 @@
 //#region @backend
-import { PathLike } from 'node:fs'
+import { PathLike } from 'node:fs';
 
-import { Memory, MemorySync } from '../adapters/Memory'
-import { JSONFile, JSONFileSync } from '../adapters/node/JSONFile'
-import { Low, LowSync } from '../core/Low'
+import { Memory, MemorySync } from '../adapters/Memory';
+import { JSONFile, JSONFileSync } from '../adapters/node/JSONFile';
+import { Low, LowSync } from '../core/Low';
 
 export async function JSONFilePreset<Data>(
   filename: PathLike,
   defaultData: Data,
+  useInMemory = false,
 ): Promise<Low<Data>> {
-  const adapter =
-    process.env.NODE_ENV === 'test'
-      ? new Memory<Data>()
-      : new JSONFile<Data>(filename)
-  const db = new Low<Data>(adapter, defaultData)
-  await db.read()
-  return db
+  const adapter = useInMemory
+    ? new Memory<Data>()
+    : new JSONFile<Data>(filename);
+  const db = new Low<Data>(adapter, defaultData);
+  await db.read();
+  return db;
 }
 
 export function JSONFileSyncPreset<Data>(
   filename: PathLike,
   defaultData: Data,
+  useInMemory = false,
 ): LowSync<Data> {
-  const adapter =
-    process.env.NODE_ENV === 'test'
-      ? new MemorySync<Data>()
-      : new JSONFileSync<Data>(filename)
-  const db = new LowSync<Data>(adapter, defaultData)
-  db.read()
-  return db
+  const adapter = useInMemory
+    ? new MemorySync<Data>()
+    : new JSONFileSync<Data>(filename);
+  const db = new LowSync<Data>(adapter, defaultData);
+  db.read();
+  return db;
 }
 //#endregion
